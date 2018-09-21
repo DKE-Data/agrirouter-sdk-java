e150d709822fb4843677c3f169223120a3571ef2
Matthias Jansen <16698244+matjansen@users.noreply.github.com>
2018-09-21 08:50:47 +0200

    added documentation for EncodedMessageResponse.kt

--- agrirouter-api-java-api/src/main/kotlin/com/dke/data/agrirouter/api/dto/encoding/EncodeMessageResponse.kt
+++ agrirouter-api-java-api/src/main/kotlin/com/dke/data/agrirouter/api/dto/encoding/EncodeMessageResponse.kt
@@ -1,3 +1,10 @@
 package com.dke.data.agrirouter.api.dto.encoding
 
+/**
+ * This class holds the data to be returned from an message encoding function
+ *
+ * @param applicationMessageID the generated application message ID
+ * @param encodedMessage the encoded message
+ *
+ */
 data class EncodeMessageResponse(val applicationMessageID: String, val encodedMessage: String)
