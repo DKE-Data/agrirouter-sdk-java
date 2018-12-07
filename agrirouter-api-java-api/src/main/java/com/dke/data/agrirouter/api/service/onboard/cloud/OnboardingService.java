af736a59e97356a1ac87c628a8aa54e7fc7d6dea
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2018-12-07 17:16:01 +0100

    Draft.

--- agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/service/onboard/cloud/OnboardingService.java
+++ agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/service/onboard/cloud/OnboardingService.java
@@ -1,7 +1,13 @@
 package com.dke.data.agrirouter.api.service.onboard.cloud;
 
 import agrirouter.cloud.registration.CloudVirtualizedAppRegistration;
+import com.dke.data.agrirouter.api.dto.onboard.OnboardingResponse;
 import com.dke.data.agrirouter.api.service.messaging.encoding.MessageDecoder;
+import com.dke.data.agrirouter.api.service.parameters.CloudOnboardingParameters;
+import java.util.List;
 
 public interface OnboardingService
-    extends MessageDecoder<CloudVirtualizedAppRegistration.OnboardingResponse> {}
+    extends MessageDecoder<CloudVirtualizedAppRegistration.OnboardingResponse> {
+
+  List<OnboardingResponse> onboard(CloudOnboardingParameters parameters);
+}
