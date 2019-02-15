9afbd172df2a2329b5bf43f6e627b056d9707e4e
Frank <frank.wiebeler@dev4agriculture.de>
2019-02-15 11:46:53 +0100

    +ADD: requestFullListFiltered und requestFullListUnfiltered to Interface

--- agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/service/messaging/EndpointsListService.java
+++ agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/service/messaging/EndpointsListService.java
@@ -1,5 +1,12 @@
 package com.dke.data.agrirouter.api.service.messaging;
 
+import agrirouter.request.payload.account.Endpoints;
+import com.dke.data.agrirouter.api.dto.onboard.OnboardingResponse;
 import com.dke.data.agrirouter.api.service.parameters.EndpointsListParameters;
 
-public interface EndpointsListService extends MessagingService<EndpointsListParameters> {}
+public interface EndpointsListService extends MessagingService<EndpointsListParameters> {
+
+  String requestFullListFiltered(OnboardingResponse onboardingResponse);
+
+  String requestFullListUnFiltered(OnboardingResponse onboardingResponse);
+}
