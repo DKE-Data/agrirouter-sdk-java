f8a50b3df635e6f91b4aba489342ff20937af7e7
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2019-07-15 15:32:58 +0200

    +ADD | Remove unused imports.

--- agrirouter-api-java-api/src/main/kotlin/com/dke/data/agrirouter/api/service/parameters/SetSubscriptionParameters.kt
+++ agrirouter-api-java-api/src/main/kotlin/com/dke/data/agrirouter/api/service/parameters/SetSubscriptionParameters.kt
@@ -1,13 +1,10 @@
 package com.dke.data.agrirouter.api.service.parameters
 
-import agrirouter.request.payload.endpoint.Capabilities
 import com.dke.data.agrirouter.api.dto.onboard.OnboardingResponse
 import com.dke.data.agrirouter.api.enums.TechnicalMessageType
-import com.dke.data.agrirouter.api.factories.impl.parameters.SubscriptionMessageParameters
 import com.dke.data.agrirouter.api.service.ParameterValidation
 import com.dke.data.agrirouter.api.service.parameters.base.AbstractParameterBase
 import lombok.ToString
-import javax.validation.constraints.NotBlank
 import javax.validation.constraints.NotNull
 
 /**
