ff3ed8b8e8d19710ae8dd4a3f65343da8ba6d3ca
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2019-07-17 16:58:57 +0200

    +ADD | Inital module.

--- agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/service/messaging/SetSubscriptionService.java
+++ agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/service/messaging/SetSubscriptionService.java
@@ -2,7 +2,5 @@ package com.dke.data.agrirouter.api.service.messaging;
 
 import com.dke.data.agrirouter.api.service.parameters.SetSubscriptionParameters;
 
-/**
- * Service to set the subscriptions for an endpoint.
- */
+/** Service to set the subscriptions for an endpoint. */
 public interface SetSubscriptionService extends MessagingService<SetSubscriptionParameters> {}
