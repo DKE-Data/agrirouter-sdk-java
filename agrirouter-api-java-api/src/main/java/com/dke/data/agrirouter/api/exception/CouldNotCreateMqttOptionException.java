df584af18f952655cb3f8cb54dcf4bd9d0190536
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2019-07-18 16:03:03 +0200

    +ADD | Format.

--- agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/exception/CouldNotCreateMqttOptionException.java
+++ agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/exception/CouldNotCreateMqttOptionException.java
@@ -1,15 +1,13 @@
 package com.dke.data.agrirouter.api.exception;
 
-/**
- * Will be thrown if the MQTT option can not be created.
- */
+/** Will be thrown if the MQTT option can not be created. */
 public class CouldNotCreateMqttOptionException extends RuntimeException {
 
-    public CouldNotCreateMqttOptionException(String message) {
-        super(message);
-    }
+  public CouldNotCreateMqttOptionException(String message) {
+    super(message);
+  }
 
-    public CouldNotCreateMqttOptionException(String message, Throwable cause) {
-        super(message, cause);
-    }
+  public CouldNotCreateMqttOptionException(String message, Throwable cause) {
+    super(message, cause);
+  }
 }
