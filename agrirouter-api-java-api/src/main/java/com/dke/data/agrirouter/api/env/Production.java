044eff32111fe92343b642bf29c9852be09777b2
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2019-10-16 18:33:12 +0200

    Format.

--- agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/env/Production.java
+++ agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/env/Production.java
@@ -1,37 +1,35 @@
 package com.dke.data.agrirouter.api.env;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
- * Abstraction of the production environment, currently no overrides because the default is production already.
+ * Abstraction of the production environment, currently no overrides because the default is
+ * production already.
  */
 public abstract class Production implements Environment {
 
-    private static final String ENV_BASE_URL = "https://goto.my-agrirouter.com";
-    private static final String API_PREFIX = "/api/v1.0";
-    private static final String REGISTRATION_SERVICE_URL =
-            "https://agrirouter-registration-service.cfapps.eu1.hana.ondemand.com";
+  private static final String ENV_BASE_URL = "https://goto.my-agrirouter.com";
+  private static final String API_PREFIX = "/api/v1.0";
+  private static final String REGISTRATION_SERVICE_URL =
+      "https://agrirouter-registration-service.cfapps.eu1.hana.ondemand.com";
 
-    @Override
-    public String getEnvironmentBaseUrl() {
-        return ENV_BASE_URL;
-    }
+  @Override
+  public String getEnvironmentBaseUrl() {
+    return ENV_BASE_URL;
+  }
 
-    @Override
-    public String getApiPrefix() {
-        return API_PREFIX;
-    }
+  @Override
+  public String getApiPrefix() {
+    return API_PREFIX;
+  }
 
-    @Override
-    public String getRegistrationServiceUrl() {
-        return REGISTRATION_SERVICE_URL;
-    }
+  @Override
+  public String getRegistrationServiceUrl() {
+    return REGISTRATION_SERVICE_URL;
+  }
 
-    @Override
-    public List<String> getRootCertificates() {
-        throw new RuntimeException("Not supported. Deprecated.");
-    }
-
-    ;
+  @Override
+  public List<String> getRootCertificates() {
+    throw new RuntimeException("Not supported. Deprecated.");
+  }
 }
