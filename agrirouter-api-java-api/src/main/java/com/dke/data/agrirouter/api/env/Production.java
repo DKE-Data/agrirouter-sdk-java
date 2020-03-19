719e4bdb60761a4e1bad1f505496baf8a4932718
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2020-03-19 10:21:38 +0100

    Fix format.

--- agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/env/Production.java
+++ agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/env/Production.java
@@ -6,22 +6,22 @@ package com.dke.data.agrirouter.api.env;
  */
 public abstract class Production implements Environment {
 
-    private static final String ENV_BASE_URL = "https://goto.my-agrirouter.com";
-    private static final String API_PREFIX = "/api/v1.0";
-    private static final String REGISTRATION_SERVICE_URL = "https://onboard.my-agrirouter.com";
+  private static final String ENV_BASE_URL = "https://goto.my-agrirouter.com";
+  private static final String API_PREFIX = "/api/v1.0";
+  private static final String REGISTRATION_SERVICE_URL = "https://onboard.my-agrirouter.com";
 
-    @Override
-    public String getEnvironmentBaseUrl() {
-        return ENV_BASE_URL;
-    }
+  @Override
+  public String getEnvironmentBaseUrl() {
+    return ENV_BASE_URL;
+  }
 
-    @Override
-    public String getApiPrefix() {
-        return API_PREFIX;
-    }
+  @Override
+  public String getApiPrefix() {
+    return API_PREFIX;
+  }
 
-    @Override
-    public String getRegistrationServiceUrl() {
-        return REGISTRATION_SERVICE_URL;
-    }
+  @Override
+  public String getRegistrationServiceUrl() {
+    return REGISTRATION_SERVICE_URL;
+  }
 }
