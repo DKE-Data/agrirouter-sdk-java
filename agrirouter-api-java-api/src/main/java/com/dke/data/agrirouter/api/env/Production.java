402043f42a1d3a4c2a5afc62786ce014df7d9e0e
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2020-03-19 11:35:08 +0100

    Add new URL to the production environment. (#102)
    * Add new URL to the production environment.
    
    * Fix format.

--- agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/env/Production.java
+++ agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/env/Production.java
@@ -8,8 +8,7 @@ public abstract class Production implements Environment {
 
   private static final String ENV_BASE_URL = "https://goto.my-agrirouter.com";
   private static final String API_PREFIX = "/api/v1.0";
-  private static final String REGISTRATION_SERVICE_URL =
-      "https://agrirouter-registration-service.cfapps.eu1.hana.ondemand.com";
+  private static final String REGISTRATION_SERVICE_URL = "https://onboard.my-agrirouter.com";
 
   @Override
   public String getEnvironmentBaseUrl() {
