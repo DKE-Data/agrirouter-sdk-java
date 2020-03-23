fca99ea633c16b5747d9ae6a685c7c7a7df314dd
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2020-03-23 09:43:52 +0100

    Fix format.

--- agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/service/messaging/SendContentMessageService.java
+++ agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/service/messaging/SendContentMessageService.java
@@ -2,23 +2,20 @@ package com.dke.data.agrirouter.api.service.messaging;
 
 import com.dke.data.agrirouter.api.service.parameters.SendContentMessageParameters;
 
-/**
- * Sending a content message to the AR.
- */
+/** Sending a content message to the AR. */
 public interface SendContentMessageService extends MessagingService<SendContentMessageParameters> {
-    /// <summary>
-    /// Maximum value the AR can handle.
-    /// </summary>
-    int MAXIMUM_SUPPORTED = 1000000 - (int) (1000000 * 0.05);
+  /// <summary>
+  /// Maximum value the AR can handle.
+  /// </summary>
+  int MAXIMUM_SUPPORTED = 1000000 - (int) (1000000 * 0.05);
 
-    /// <summary>
-    /// Half of a megabyte.
-    /// </summary>
-    int HALF_OF_THE_MAXIMUM = MAXIMUM_SUPPORTED / 2;
+  /// <summary>
+  /// Half of a megabyte.
+  /// </summary>
+  int HALF_OF_THE_MAXIMUM = MAXIMUM_SUPPORTED / 2;
 
-    /// <summary>
-    /// Quarter of a megabyte.
-    /// </summary>
-    int QUARTER_OF_THE_MAXIMUM = MAXIMUM_SUPPORTED / 4;
-
+  /// <summary>
+  /// Quarter of a megabyte.
+  /// </summary>
+  int QUARTER_OF_THE_MAXIMUM = MAXIMUM_SUPPORTED / 4;
 }
--- agrirouter-api-java-impl/src/main/java/com/dke/data/agrirouter/impl/common/SequenceNumberService.java
+++ agrirouter-api-java-impl/src/main/java/com/dke/data/agrirouter/impl/common/SequenceNumberService.java
@@ -2,20 +2,17 @@ package com.dke.data.agrirouter.impl.common;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-/**
- * Message for sequence number generation.
- */
+/** Message for sequence number generation. */
 public class SequenceNumberService {
 
-    private static final AtomicLong globalSeqNo = new AtomicLong();
+  private static final AtomicLong globalSeqNo = new AtomicLong();
 
-    /**
-     * Get the next sequence number for sending messages.
-     *
-     * @return -
-     */
-    public static long next() {
-        return globalSeqNo.addAndGet(1);
-    }
-
+  /**
+   * Get the next sequence number for sending messages.
+   *
+   * @return -
+   */
+  public static long next() {
+    return globalSeqNo.addAndGet(1);
+  }
 }
