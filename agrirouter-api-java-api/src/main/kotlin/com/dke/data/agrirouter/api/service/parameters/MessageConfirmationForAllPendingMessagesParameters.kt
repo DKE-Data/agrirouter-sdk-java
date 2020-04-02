59ce480a73ea192f60a6b2acaf44254558a3c543
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2020-04-02 17:13:05 +0200

    Customize parameter validation.

--- agrirouter-api-java-api/src/main/kotlin/com/dke/data/agrirouter/api/service/parameters/MessageConfirmationForAllPendingMessagesParameters.kt
+++ agrirouter-api-java-api/src/main/kotlin/com/dke/data/agrirouter/api/service/parameters/MessageConfirmationForAllPendingMessagesParameters.kt
@@ -3,14 +3,16 @@ package com.dke.data.agrirouter.api.service.parameters
 import com.dke.data.agrirouter.api.dto.onboard.OnboardingResponse
 import com.dke.data.agrirouter.api.service.ParameterValidation
 import com.dke.data.agrirouter.api.service.parameters.base.AbstractParameterBase
-import javax.validation.constraints.NotNull
 
 /**
  * Parameters class. Encapsulation for the services.
  */
 class MessageConfirmationForAllPendingMessagesParameters : AbstractParameterBase(), ParameterValidation {
 
-    @NotNull
     lateinit var onboardingResponse: OnboardingResponse
+
+    override fun technicalValidation() {
+        nullCheck(onboardingResponse)
+    }
 
 }
