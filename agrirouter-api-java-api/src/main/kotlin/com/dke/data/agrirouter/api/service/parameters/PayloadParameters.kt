33ce9a8c9c859eb48fd861b8d703561c75e707e4
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2020-04-02 17:22:48 +0200

    Customize parameter validation.

--- agrirouter-api-java-api/src/main/kotlin/com/dke/data/agrirouter/api/service/parameters/PayloadParameters.kt
+++ agrirouter-api-java-api/src/main/kotlin/com/dke/data/agrirouter/api/service/parameters/PayloadParameters.kt
@@ -3,17 +3,19 @@ package com.dke.data.agrirouter.api.service.parameters
 import com.dke.data.agrirouter.api.service.ParameterValidation
 import com.dke.data.agrirouter.api.service.parameters.base.AbstractParameterBase
 import com.google.protobuf.ByteString
-import javax.validation.constraints.NotNull
 
 /**
  * Parameters class. Encapsulation for the services.
  */
 class PayloadParameters : AbstractParameterBase(), ParameterValidation {
 
-    @NotNull
     var typeUrl: String = ""
 
-    @NotNull
     lateinit var value: ByteString
+
+    override fun technicalValidation() {
+        isBlank(typeUrl)
+        nullCheck(value)
+    }
 
 }
