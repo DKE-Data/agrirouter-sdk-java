49353ea9b9b2e043e0e47293ad9484fbd5d5ab6a
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2020-04-07 14:53:04 +0200

    Implement demonstration how to use the service for settings capabilities.

---
+++ agrirouter-api-tests/src/test/java/com/dke/data/agrirouter/test/Assertions.java
@@ -0,0 +1,29 @@
+package com.dke.data.agrirouter.test;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/** Custom assertion adapter. */
+public class Assertions extends org.junit.jupiter.api.Assertions {
+
+  /**
+   * Assert that the actual value matches any from the given expected values.
+   *
+   * @param expectedValues -
+   * @param actual -
+   * @param <T> -
+   */
+  public static <T> void assertMatchesAny(List<T> expectedValues, T actual) {
+    AtomicBoolean matches = new AtomicBoolean(false);
+    assertNotNull(expectedValues);
+    assertNotNull(actual);
+    expectedValues.forEach(
+        (expected) -> {
+          assertNotNull(expected);
+          if (expected.equals(actual)) {
+            matches.set(true);
+          }
+        });
+    assertTrue(matches.get(), "There should be at least a single match in the expected values.");
+  }
+}
