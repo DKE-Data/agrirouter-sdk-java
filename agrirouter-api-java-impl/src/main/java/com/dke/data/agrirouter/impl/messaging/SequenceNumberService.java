adaaf8e891b2409cef7041aa5800717bdd35b5ed
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2020-11-25 07:49:32 +0100

    Format. Assertions for NULL checks.

--- agrirouter-api-java-impl/src/main/java/com/dke/data/agrirouter/impl/messaging/SequenceNumberService.java
+++ agrirouter-api-java-impl/src/main/java/com/dke/data/agrirouter/impl/messaging/SequenceNumberService.java
@@ -1,28 +1,31 @@
 package com.dke.data.agrirouter.impl.messaging;
 
 import com.dke.data.agrirouter.api.dto.onboard.OnboardingResponse;
-
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * Service to generate sequence numbers while sending messages to the agrirouter.
- * The sequence number generation is based on the ID of the endpoint, therefore a sequence number can be used multiple times for different endpoints.
+ * Service to generate sequence numbers while sending messages to the agrirouter. The sequence
+ * number generation is based on the ID of the endpoint, therefore a sequence number can be used
+ * multiple times for different endpoints.
  */
 public class SequenceNumberService {
 
-    private static final ConcurrentHashMap<String, Long> sequenceNumbersForEndpoints = new ConcurrentHashMap<>();
+  private static final ConcurrentHashMap<String, Long> sequenceNumbersForEndpoints =
+      new ConcurrentHashMap<>();
 
-    /**
-     * Generate the sequence number for the onboarding response.
-     *
-     * @param onboardingResponse -
-     * @return 1 if this was the first call, 1+n for the n-th call.
-     */
-    public static synchronized long generateSequenceNumberForEndpoint(OnboardingResponse onboardingResponse) {
-        sequenceNumbersForEndpoints.putIfAbsent(onboardingResponse.getSensorAlternateId(), 1L);
-        Long currentSequenceNumber = sequenceNumbersForEndpoints.get(onboardingResponse.getSensorAlternateId());
-        sequenceNumbersForEndpoints.put(onboardingResponse.getSensorAlternateId(), currentSequenceNumber + 1);
-        return currentSequenceNumber;
-    }
-
+  /**
+   * Generate the sequence number for the onboarding response.
+   *
+   * @param onboardingResponse -
+   * @return 1 if this was the first call, 1+n for the n-th call.
+   */
+  public static synchronized long generateSequenceNumberForEndpoint(
+      OnboardingResponse onboardingResponse) {
+    sequenceNumbersForEndpoints.putIfAbsent(onboardingResponse.getSensorAlternateId(), 1L);
+    Long currentSequenceNumber =
+        sequenceNumbersForEndpoints.get(onboardingResponse.getSensorAlternateId());
+    sequenceNumbersForEndpoints.put(
+        onboardingResponse.getSensorAlternateId(), currentSequenceNumber + 1);
+    return currentSequenceNumber;
+  }
 }
