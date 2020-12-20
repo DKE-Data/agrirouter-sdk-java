b0a236ec702f497db5028e576d68bf798a8ed0be
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2020-12-20 14:16:09 +0100

    Add cancellation token for a better implementation.

--- agrirouter-api-java-impl/src/main/java/com/dke/data/agrirouter/impl/messaging/rest/MessageFetcher.java
+++ agrirouter-api-java-impl/src/main/java/com/dke/data/agrirouter/impl/messaging/rest/MessageFetcher.java
@@ -3,42 +3,64 @@ package com.dke.data.agrirouter.impl.messaging.rest;
 import com.dke.data.agrirouter.api.enums.CertificationType;
 import com.dke.data.agrirouter.api.service.parameters.FetchMessageParameters;
 import com.dke.data.agrirouter.impl.RequestFactory;
+import com.dke.data.agrirouter.impl.messaging.rest.cancellation.CancellationToken;
+import com.dke.data.agrirouter.impl.messaging.rest.cancellation.DefaultCancellationToken;
 import com.dke.data.agrirouter.impl.validation.ResponseValidator;
-import java.util.Optional;
-import javax.ws.rs.core.Response;
 import org.apache.commons.lang3.StringUtils;
 
+import javax.ws.rs.core.Response;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * Interface to fetch messages for the HTTP implementation by polling the outbox.
+ */
 public interface MessageFetcher extends ResponseValidator {
 
-  int MAX_TRIES_BEFORE_FAILURE = 10;
-  long DEFAULT_INTERVAL = 500;
+    int MAX_TRIES_BEFORE_FAILURE = 10;
+    long DEFAULT_INTERVAL = 500;
 
-  String EMPTY_CONTENT = "[]";
+    String EMPTY_CONTENT = "[]";
 
-  default Optional<String> poll(FetchMessageParameters parameters, int maxTries, long interval) {
-    parameters.validate();
-    int nrOfTries = 0;
-    while (nrOfTries < maxTries) {
-      Response response =
-          RequestFactory.securedRequest(
-                  parameters.getOnboardingResponse().getConnectionCriteria().getCommands(),
-                  parameters.getOnboardingResponse().getAuthentication().getCertificate(),
-                  parameters.getOnboardingResponse().getAuthentication().getSecret(),
-                  CertificationType.valueOf(
-                      parameters.getOnboardingResponse().getAuthentication().getType()))
-              .get();
-      this.assertStatusCodeIsOk(response.getStatus());
-      String entityContent = response.readEntity(String.class);
-      if (!StringUtils.equalsIgnoreCase(entityContent, EMPTY_CONTENT)) {
-        return Optional.of(entityContent);
-      }
-      nrOfTries++;
-      try {
-        Thread.sleep(interval);
-      } catch (InterruptedException nop) {
-        // NOP
-      }
+    /**
+     * Poll for messages using the given parameters.
+     *
+     * @param fetchMessageParameters -
+     * @param maxTries               Maximum number of tries before cancelling polling.
+     * @param interval               Time interval between the tries.
+     * @return Response from the outbox, if existing.
+     */
+    @Deprecated
+    default Optional<String> poll(FetchMessageParameters fetchMessageParameters, int maxTries, long interval) {
+        return poll(fetchMessageParameters, new DefaultCancellationToken(maxTries, interval));
     }
-    return Optional.empty();
-  }
+
+    /**
+     * Poll for new messages using the given parameters.
+     *
+     * @param fetchMessageParameters -
+     * @param cancellationToken      Token to manage the whole polling process.
+     * @return Response from the outbox, if existing.
+     */
+    default Optional<String> poll(FetchMessageParameters fetchMessageParameters, CancellationToken cancellationToken) {
+        fetchMessageParameters.validate();
+        while (cancellationToken.isNotCancelled()) {
+            Response response =
+                    RequestFactory.securedRequest(
+                            Objects.requireNonNull(fetchMessageParameters.getOnboardingResponse()).getConnectionCriteria().getCommands(),
+                            fetchMessageParameters.getOnboardingResponse().getAuthentication().getCertificate(),
+                            fetchMessageParameters.getOnboardingResponse().getAuthentication().getSecret(),
+                            CertificationType.valueOf(
+                                    fetchMessageParameters.getOnboardingResponse().getAuthentication().getType()))
+                            .get();
+            this.assertStatusCodeIsOk(response.getStatus());
+            String entityContent = response.readEntity(String.class);
+            if (!StringUtils.equalsIgnoreCase(entityContent, EMPTY_CONTENT)) {
+                return Optional.of(entityContent);
+            }
+            cancellationToken.step();
+            cancellationToken.waitBeforeStartingTheNextStep();
+        }
+        return Optional.empty();
+    }
 }
---
+++ agrirouter-api-java-impl/src/main/java/com/dke/data/agrirouter/impl/messaging/rest/cancellation/DefaultCancellationToken.java
@@ -0,0 +1,36 @@
+package com.dke.data.agrirouter.impl.messaging.rest.cancellation;
+
+/**
+ * Default - time based - implementation of the cancellation token.
+ */
+public class DefaultCancellationToken implements CancellationToken {
+
+    private int nrOfTries;
+    private final int maxTries;
+    private final long timeToWait;
+
+    public DefaultCancellationToken(int maxTries, long timeToWait) {
+        this.nrOfTries = 0;
+        this.maxTries = maxTries;
+        this.timeToWait = timeToWait;
+    }
+
+    @Override
+    public boolean isNotCancelled() {
+        return nrOfTries < maxTries;
+    }
+
+    @Override
+    public void step() {
+        nrOfTries++;
+    }
+
+    @Override
+    public void waitBeforeStartingTheNextStep() {
+        try {
+            Thread.sleep(timeToWait);
+        } catch (InterruptedException nop) {
+            // NOP
+        }
+    }
+}
