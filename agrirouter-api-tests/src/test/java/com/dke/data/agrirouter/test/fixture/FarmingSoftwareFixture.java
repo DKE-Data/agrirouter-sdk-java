b172aae0c3c72957a8acd8a896c943a78c27b3c0
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2020-12-23 20:25:58 +0100

    Format.

--- agrirouter-api-tests/src/test/java/com/dke/data/agrirouter/test/fixture/FarmingSoftwareFixture.java
+++ agrirouter-api-tests/src/test/java/com/dke/data/agrirouter/test/fixture/FarmingSoftwareFixture.java
@@ -1,5 +1,7 @@
 package com.dke.data.agrirouter.test.fixture;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import com.dke.data.agrirouter.api.dto.onboard.OnboardingResponse;
 import com.dke.data.agrirouter.api.enums.CertificationType;
 import com.dke.data.agrirouter.api.enums.Gateway;
@@ -9,119 +11,113 @@ import com.dke.data.agrirouter.api.service.parameters.SecuredOnboardingParameter
 import com.dke.data.agrirouter.impl.onboard.secured.OnboardingServiceImpl;
 import com.dke.data.agrirouter.test.AbstractIntegrationTest;
 import com.dke.data.agrirouter.test.OnboardingResponseRepository;
+import java.io.IOException;
+import java.util.UUID;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-/**
- * Class to onboard endpoints for different reasons.
- */
+/** Class to onboard endpoints for different reasons. */
 class FarmingSoftwareFixture extends AbstractIntegrationTest {
 
-    /**
-     * Create a new registration token by using the following link:
-     * <p>
-     * https://agrirouter-qa.cfapps.eu10.hana.ondemand.com/application/905152eb-c526-47a3-b871-aa46d065bb4c/authorize?response_type=onboard&state=my-custom-state-to-identify-the-request&redirect_uri=
-     */
-    @Test
-    @Disabled("Please replace the placeholder for the registration code to run the test case.")
-    void onboardFarmingSoftwareAndSaveToFile() throws IOException {
-        OnboardingService onboardingService = new OnboardingServiceImpl(new QA() {
-        });
-        SecuredOnboardingParameters onboardingParameters = new SecuredOnboardingParameters();
-        onboardingParameters.setRegistrationCode("e757d7aff5");
-        onboardingParameters.setApplicationId(farmingSoftware.getApplicationId());
-        onboardingParameters.setCertificationVersionId(farmingSoftware.getCertificationVersionId());
-        onboardingParameters.setCertificationType(CertificationType.P12);
-        onboardingParameters.setGatewayId(Gateway.REST.getKey());
-        onboardingParameters.setUuid(UUID.randomUUID().toString());
-        onboardingParameters.setPrivateKey(farmingSoftware.getPrivateKey());
-        onboardingParameters.setPublicKey(farmingSoftware.getPublicKey());
-        final OnboardingResponse onboardingResponse = onboardingService.onboard(onboardingParameters);
-        assertNotNull(onboardingResponse);
-        assertNotNull(onboardingResponse.getCapabilityAlternateId());
-        assertNotNull(onboardingResponse.getDeviceAlternateId());
-        assertNotNull(onboardingResponse.getSensorAlternateId());
-        assertNotNull(onboardingResponse.getAuthentication());
-        assertNotNull(onboardingResponse.getAuthentication().getCertificate());
-        assertNotNull(onboardingResponse.getAuthentication().getSecret());
-        assertNotNull(onboardingResponse.getAuthentication().getType());
-        assertNotNull(onboardingResponse.getConnectionCriteria());
-        assertNotNull(onboardingResponse.getConnectionCriteria().getMeasures());
-        assertNotNull(onboardingResponse.getConnectionCriteria().getCommands());
-        OnboardingResponseRepository.save(OnboardingResponseRepository.Identifier.FARMING_SOFTWARE, onboardingResponse);
-    }
+  /**
+   * Create a new registration token by using the following link:
+   *
+   * <p>https://agrirouter-qa.cfapps.eu10.hana.ondemand.com/application/905152eb-c526-47a3-b871-aa46d065bb4c/authorize?response_type=onboard&state=my-custom-state-to-identify-the-request&redirect_uri=
+   */
+  @Test
+  @Disabled("Please replace the placeholder for the registration code to run the test case.")
+  void onboardFarmingSoftwareAndSaveToFile() throws IOException {
+    OnboardingService onboardingService = new OnboardingServiceImpl(new QA() {});
+    SecuredOnboardingParameters onboardingParameters = new SecuredOnboardingParameters();
+    onboardingParameters.setRegistrationCode("e757d7aff5");
+    onboardingParameters.setApplicationId(farmingSoftware.getApplicationId());
+    onboardingParameters.setCertificationVersionId(farmingSoftware.getCertificationVersionId());
+    onboardingParameters.setCertificationType(CertificationType.P12);
+    onboardingParameters.setGatewayId(Gateway.REST.getKey());
+    onboardingParameters.setUuid(UUID.randomUUID().toString());
+    onboardingParameters.setPrivateKey(farmingSoftware.getPrivateKey());
+    onboardingParameters.setPublicKey(farmingSoftware.getPublicKey());
+    final OnboardingResponse onboardingResponse = onboardingService.onboard(onboardingParameters);
+    assertNotNull(onboardingResponse);
+    assertNotNull(onboardingResponse.getCapabilityAlternateId());
+    assertNotNull(onboardingResponse.getDeviceAlternateId());
+    assertNotNull(onboardingResponse.getSensorAlternateId());
+    assertNotNull(onboardingResponse.getAuthentication());
+    assertNotNull(onboardingResponse.getAuthentication().getCertificate());
+    assertNotNull(onboardingResponse.getAuthentication().getSecret());
+    assertNotNull(onboardingResponse.getAuthentication().getType());
+    assertNotNull(onboardingResponse.getConnectionCriteria());
+    assertNotNull(onboardingResponse.getConnectionCriteria().getMeasures());
+    assertNotNull(onboardingResponse.getConnectionCriteria().getCommands());
+    OnboardingResponseRepository.save(
+        OnboardingResponseRepository.Identifier.FARMING_SOFTWARE, onboardingResponse);
+  }
 
-    /**
-     * Create a new registration token by using the following link:
-     * <p>
-     * https://agrirouter-qa.cfapps.eu10.hana.ondemand.com/application/905152eb-c526-47a3-b871-aa46d065bb4c/authorize?response_type=onboard&state=my-custom-state-to-identify-the-request&redirect_uri=
-     */
-    @Test
-    @Disabled("Please replace the placeholder for the registration code to run the test case.")
-    void onboardAndDeactivateFarmingSoftwareAndSaveToFile() throws IOException {
-        OnboardingService onboardingService = new OnboardingServiceImpl(new QA() {
-        });
-        SecuredOnboardingParameters onboardingParameters = new SecuredOnboardingParameters();
-        onboardingParameters.setRegistrationCode("aff2b14ef6");
-        onboardingParameters.setApplicationId(farmingSoftware.getApplicationId());
-        onboardingParameters.setCertificationVersionId(farmingSoftware.getCertificationVersionId());
-        onboardingParameters.setCertificationType(CertificationType.P12);
-        onboardingParameters.setGatewayId(Gateway.REST.getKey());
-        onboardingParameters.setUuid(UUID.randomUUID().toString());
-        onboardingParameters.setPrivateKey(farmingSoftware.getPrivateKey());
-        onboardingParameters.setPublicKey(farmingSoftware.getPublicKey());
-        final OnboardingResponse onboardingResponse = onboardingService.onboard(onboardingParameters);
-        assertNotNull(onboardingResponse);
-        assertNotNull(onboardingResponse.getCapabilityAlternateId());
-        assertNotNull(onboardingResponse.getDeviceAlternateId());
-        assertNotNull(onboardingResponse.getSensorAlternateId());
-        assertNotNull(onboardingResponse.getAuthentication());
-        assertNotNull(onboardingResponse.getAuthentication().getCertificate());
-        assertNotNull(onboardingResponse.getAuthentication().getSecret());
-        assertNotNull(onboardingResponse.getAuthentication().getType());
-        assertNotNull(onboardingResponse.getConnectionCriteria());
-        assertNotNull(onboardingResponse.getConnectionCriteria().getMeasures());
-        assertNotNull(onboardingResponse.getConnectionCriteria().getCommands());
-        OnboardingResponseRepository.save(OnboardingResponseRepository.Identifier.FARMING_SOFTWARE_DEACTIVATED, onboardingResponse);
-    }
+  /**
+   * Create a new registration token by using the following link:
+   *
+   * <p>https://agrirouter-qa.cfapps.eu10.hana.ondemand.com/application/905152eb-c526-47a3-b871-aa46d065bb4c/authorize?response_type=onboard&state=my-custom-state-to-identify-the-request&redirect_uri=
+   */
+  @Test
+  @Disabled("Please replace the placeholder for the registration code to run the test case.")
+  void onboardAndDeactivateFarmingSoftwareAndSaveToFile() throws IOException {
+    OnboardingService onboardingService = new OnboardingServiceImpl(new QA() {});
+    SecuredOnboardingParameters onboardingParameters = new SecuredOnboardingParameters();
+    onboardingParameters.setRegistrationCode("aff2b14ef6");
+    onboardingParameters.setApplicationId(farmingSoftware.getApplicationId());
+    onboardingParameters.setCertificationVersionId(farmingSoftware.getCertificationVersionId());
+    onboardingParameters.setCertificationType(CertificationType.P12);
+    onboardingParameters.setGatewayId(Gateway.REST.getKey());
+    onboardingParameters.setUuid(UUID.randomUUID().toString());
+    onboardingParameters.setPrivateKey(farmingSoftware.getPrivateKey());
+    onboardingParameters.setPublicKey(farmingSoftware.getPublicKey());
+    final OnboardingResponse onboardingResponse = onboardingService.onboard(onboardingParameters);
+    assertNotNull(onboardingResponse);
+    assertNotNull(onboardingResponse.getCapabilityAlternateId());
+    assertNotNull(onboardingResponse.getDeviceAlternateId());
+    assertNotNull(onboardingResponse.getSensorAlternateId());
+    assertNotNull(onboardingResponse.getAuthentication());
+    assertNotNull(onboardingResponse.getAuthentication().getCertificate());
+    assertNotNull(onboardingResponse.getAuthentication().getSecret());
+    assertNotNull(onboardingResponse.getAuthentication().getType());
+    assertNotNull(onboardingResponse.getConnectionCriteria());
+    assertNotNull(onboardingResponse.getConnectionCriteria().getMeasures());
+    assertNotNull(onboardingResponse.getConnectionCriteria().getCommands());
+    OnboardingResponseRepository.save(
+        OnboardingResponseRepository.Identifier.FARMING_SOFTWARE_DEACTIVATED, onboardingResponse);
+  }
 
-    /**
-     * Create a new registration token by using the following link:
-     * <p>
-     * https://agrirouter-qa.cfapps.eu10.hana.ondemand.com/application/905152eb-c526-47a3-b871-aa46d065bb4c/authorize?response_type=onboard&state=my-custom-state-to-identify-the-request&redirect_uri=
-     */
-    @Test
-    @Disabled("Please replace the placeholder for the registration code to run the test case.")
-    void onboardAndRemoveFarmingSoftwareAndSaveToFile() throws IOException {
-        OnboardingService onboardingService = new OnboardingServiceImpl(new QA() {
-        });
-        SecuredOnboardingParameters onboardingParameters = new SecuredOnboardingParameters();
-        onboardingParameters.setRegistrationCode("cf968d4af7");
-        onboardingParameters.setApplicationId(farmingSoftware.getApplicationId());
-        onboardingParameters.setCertificationVersionId(farmingSoftware.getCertificationVersionId());
-        onboardingParameters.setCertificationType(CertificationType.P12);
-        onboardingParameters.setGatewayId(Gateway.REST.getKey());
-        onboardingParameters.setUuid(UUID.randomUUID().toString());
-        onboardingParameters.setPrivateKey(farmingSoftware.getPrivateKey());
-        onboardingParameters.setPublicKey(farmingSoftware.getPublicKey());
-        final OnboardingResponse onboardingResponse = onboardingService.onboard(onboardingParameters);
-        assertNotNull(onboardingResponse);
-        assertNotNull(onboardingResponse.getCapabilityAlternateId());
-        assertNotNull(onboardingResponse.getDeviceAlternateId());
-        assertNotNull(onboardingResponse.getSensorAlternateId());
-        assertNotNull(onboardingResponse.getAuthentication());
-        assertNotNull(onboardingResponse.getAuthentication().getCertificate());
-        assertNotNull(onboardingResponse.getAuthentication().getSecret());
-        assertNotNull(onboardingResponse.getAuthentication().getType());
-        assertNotNull(onboardingResponse.getConnectionCriteria());
-        assertNotNull(onboardingResponse.getConnectionCriteria().getMeasures());
-        assertNotNull(onboardingResponse.getConnectionCriteria().getCommands());
-        OnboardingResponseRepository.save(OnboardingResponseRepository.Identifier.FARMING_SOFTWARE_REMOVED, onboardingResponse);
-    }
-
+  /**
+   * Create a new registration token by using the following link:
+   *
+   * <p>https://agrirouter-qa.cfapps.eu10.hana.ondemand.com/application/905152eb-c526-47a3-b871-aa46d065bb4c/authorize?response_type=onboard&state=my-custom-state-to-identify-the-request&redirect_uri=
+   */
+  @Test
+  @Disabled("Please replace the placeholder for the registration code to run the test case.")
+  void onboardAndRemoveFarmingSoftwareAndSaveToFile() throws IOException {
+    OnboardingService onboardingService = new OnboardingServiceImpl(new QA() {});
+    SecuredOnboardingParameters onboardingParameters = new SecuredOnboardingParameters();
+    onboardingParameters.setRegistrationCode("cf968d4af7");
+    onboardingParameters.setApplicationId(farmingSoftware.getApplicationId());
+    onboardingParameters.setCertificationVersionId(farmingSoftware.getCertificationVersionId());
+    onboardingParameters.setCertificationType(CertificationType.P12);
+    onboardingParameters.setGatewayId(Gateway.REST.getKey());
+    onboardingParameters.setUuid(UUID.randomUUID().toString());
+    onboardingParameters.setPrivateKey(farmingSoftware.getPrivateKey());
+    onboardingParameters.setPublicKey(farmingSoftware.getPublicKey());
+    final OnboardingResponse onboardingResponse = onboardingService.onboard(onboardingParameters);
+    assertNotNull(onboardingResponse);
+    assertNotNull(onboardingResponse.getCapabilityAlternateId());
+    assertNotNull(onboardingResponse.getDeviceAlternateId());
+    assertNotNull(onboardingResponse.getSensorAlternateId());
+    assertNotNull(onboardingResponse.getAuthentication());
+    assertNotNull(onboardingResponse.getAuthentication().getCertificate());
+    assertNotNull(onboardingResponse.getAuthentication().getSecret());
+    assertNotNull(onboardingResponse.getAuthentication().getType());
+    assertNotNull(onboardingResponse.getConnectionCriteria());
+    assertNotNull(onboardingResponse.getConnectionCriteria().getMeasures());
+    assertNotNull(onboardingResponse.getConnectionCriteria().getCommands());
+    OnboardingResponseRepository.save(
+        OnboardingResponseRepository.Identifier.FARMING_SOFTWARE_REMOVED, onboardingResponse);
+  }
 }
--- agrirouter-api-tests/src/test/java/com/dke/data/agrirouter/test/messaging/rest/HealthCheckForEndpointsByPollingTest.java
+++ agrirouter-api-tests/src/test/java/com/dke/data/agrirouter/test/messaging/rest/HealthCheckForEndpointsByPollingTest.java
@@ -9,39 +9,59 @@ import com.dke.data.agrirouter.impl.messaging.rest.FetchMessageServiceImpl;
 import com.dke.data.agrirouter.impl.messaging.rest.MessageFetcher;
 import com.dke.data.agrirouter.test.AbstractIntegrationTest;
 import com.dke.data.agrirouter.test.OnboardingResponseRepository;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-/**
- * Class to show how health checking via polling can be implemented.
- */
+/** Class to show how health checking via polling can be implemented. */
 class HealthCheckForEndpointsByPollingTest extends AbstractIntegrationTest {
 
-    @Test
-    void givenExistingAndActivatedEndpointWhenPollingTheOutboxThenTheAgrirouterShouldReturnAnEmptyResult() throws IOException {
-        final OnboardingResponse onboardingResponse = OnboardingResponseRepository.read(OnboardingResponseRepository.Identifier.FARMING_SOFTWARE);
-        FetchMessageService fetchMessageService = new FetchMessageServiceImpl();
-        final Optional<List<FetchMessageResponse>> optionalMessageResponses = fetchMessageService.fetch(onboardingResponse, new DefaultCancellationToken(MessageFetcher.MAX_TRIES_BEFORE_FAILURE, MessageFetcher.DEFAULT_INTERVAL));
-        Assertions.assertFalse(optionalMessageResponses.isPresent());
-    }
+  @Test
+  void
+      givenExistingAndActivatedEndpointWhenPollingTheOutboxThenTheAgrirouterShouldReturnAnEmptyResult()
+          throws IOException {
+    final OnboardingResponse onboardingResponse =
+        OnboardingResponseRepository.read(OnboardingResponseRepository.Identifier.FARMING_SOFTWARE);
+    FetchMessageService fetchMessageService = new FetchMessageServiceImpl();
+    final Optional<List<FetchMessageResponse>> optionalMessageResponses =
+        fetchMessageService.fetch(
+            onboardingResponse,
+            new DefaultCancellationToken(
+                MessageFetcher.MAX_TRIES_BEFORE_FAILURE, MessageFetcher.DEFAULT_INTERVAL));
+    Assertions.assertFalse(optionalMessageResponses.isPresent());
+  }
 
-    @Test
-    void givenExistingAndDeactivatedEndpointWhenPollingTheOutboxThenTheAgrirouterShouldReturnAnEmptyResult() throws IOException {
-        final OnboardingResponse onboardingResponse = OnboardingResponseRepository.read(OnboardingResponseRepository.Identifier.FARMING_SOFTWARE_DEACTIVATED);
-        FetchMessageService fetchMessageService = new FetchMessageServiceImpl();
-        final Optional<List<FetchMessageResponse>> optionalMessageResponses = fetchMessageService.fetch(onboardingResponse, new DefaultCancellationToken(MessageFetcher.MAX_TRIES_BEFORE_FAILURE, MessageFetcher.DEFAULT_INTERVAL));
-        Assertions.assertFalse(optionalMessageResponses.isPresent());
-    }
+  @Test
+  void
+      givenExistingAndDeactivatedEndpointWhenPollingTheOutboxThenTheAgrirouterShouldReturnAnEmptyResult()
+          throws IOException {
+    final OnboardingResponse onboardingResponse =
+        OnboardingResponseRepository.read(
+            OnboardingResponseRepository.Identifier.FARMING_SOFTWARE_DEACTIVATED);
+    FetchMessageService fetchMessageService = new FetchMessageServiceImpl();
+    final Optional<List<FetchMessageResponse>> optionalMessageResponses =
+        fetchMessageService.fetch(
+            onboardingResponse,
+            new DefaultCancellationToken(
+                MessageFetcher.MAX_TRIES_BEFORE_FAILURE, MessageFetcher.DEFAULT_INTERVAL));
+    Assertions.assertFalse(optionalMessageResponses.isPresent());
+  }
 
-    @Test
-    void givenRemovedEndpointWhenPollingTheOutboxThenTheAgrirouterShouldDenyAccessToTheApi() throws IOException {
-        final OnboardingResponse onboardingResponse = OnboardingResponseRepository.read(OnboardingResponseRepository.Identifier.FARMING_SOFTWARE_REMOVED);
-        FetchMessageService fetchMessageService = new FetchMessageServiceImpl();
-        Assertions.assertThrows(UnauthorizedRequestException.class, () -> fetchMessageService.fetch(onboardingResponse, new DefaultCancellationToken(MessageFetcher.MAX_TRIES_BEFORE_FAILURE, MessageFetcher.DEFAULT_INTERVAL)));
-    }
-
+  @Test
+  void givenRemovedEndpointWhenPollingTheOutboxThenTheAgrirouterShouldDenyAccessToTheApi()
+      throws IOException {
+    final OnboardingResponse onboardingResponse =
+        OnboardingResponseRepository.read(
+            OnboardingResponseRepository.Identifier.FARMING_SOFTWARE_REMOVED);
+    FetchMessageService fetchMessageService = new FetchMessageServiceImpl();
+    Assertions.assertThrows(
+        UnauthorizedRequestException.class,
+        () ->
+            fetchMessageService.fetch(
+                onboardingResponse,
+                new DefaultCancellationToken(
+                    MessageFetcher.MAX_TRIES_BEFORE_FAILURE, MessageFetcher.DEFAULT_INTERVAL)));
+  }
 }
