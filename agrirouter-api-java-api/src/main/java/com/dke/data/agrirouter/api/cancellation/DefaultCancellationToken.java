458bd41be1bf918de3960b923469178eec43a551
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2021-01-12 15:15:31 +0100

    Handle `InterruptedException` gracefully.

--- agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/cancellation/DefaultCancellationToken.java
+++ agrirouter-api-java-api/src/main/java/com/dke/data/agrirouter/api/cancellation/DefaultCancellationToken.java
@@ -1,34 +1,38 @@
 package com.dke.data.agrirouter.api.cancellation;
 
-/** Default - time based - implementation of the cancellation token. */
+/**
+ * Default - time based - implementation of the cancellation token.
+ */
 public class DefaultCancellationToken implements CancellationToken {
 
-  private int nrOfTries;
-  private final int maxTries;
-  private final long timeToWait;
+    private int nrOfTries;
+    private final int maxTries;
+    private final long timeToWait;
+    private boolean wasInterrupted;
 
-  public DefaultCancellationToken(int maxTries, long timeToWait) {
-    this.nrOfTries = 0;
-    this.maxTries = maxTries;
-    this.timeToWait = timeToWait;
-  }
-
-  @Override
-  public boolean isNotCancelled() {
-    return nrOfTries < maxTries;
-  }
-
-  @Override
-  public void step() {
-    nrOfTries++;
-  }
-
-  @Override
-  public void waitBeforeStartingNextStep() {
-    try {
-      Thread.sleep(timeToWait);
-    } catch (InterruptedException nop) {
-      // NOP
+    public DefaultCancellationToken(int maxTries, long timeToWait) {
+        this.nrOfTries = 0;
+        this.maxTries = maxTries;
+        this.timeToWait = timeToWait;
+        this.wasInterrupted = false;
     }
-  }
+
+    @Override
+    public boolean isNotCancelled() {
+        return nrOfTries < maxTries && !wasInterrupted;
+    }
+
+    @Override
+    public void step() {
+        nrOfTries++;
+    }
+
+    @Override
+    public void waitBeforeStartingNextStep() {
+        try {
+            Thread.sleep(timeToWait);
+        } catch (InterruptedException nop) {
+            wasInterrupted = true;
+        }
+    }
 }
