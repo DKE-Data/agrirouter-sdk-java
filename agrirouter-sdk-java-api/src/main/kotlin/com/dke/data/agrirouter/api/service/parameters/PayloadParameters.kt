9e5bb337f87da81ed4bf563d2373f9dbc42a52f1
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2021-12-16 16:09:16 +0100

    Fix typo.

--- agrirouter-sdk-java-api/src/main/kotlin/com/dke/data/agrirouter/api/service/parameters/PayloadParameters.kt
+++ agrirouter-sdk-java-api/src/main/kotlin/com/dke/data/agrirouter/api/service/parameters/PayloadParameters.kt
@@ -9,7 +9,7 @@ import com.google.protobuf.ByteString
  * - The maximum body size is equivalent of 1024000 characters/signs
  * - Since the chunking is performed on the raw message data this means, that we have to lower the MAX_LENGTH_FOR_MESSAGES to allow Base64 encoding afterwards.
  * - Total message size is limited to 1468000 characters/signs
- * - Message that are above this limit will be rejected.
+ * - Messages that are above this limit will be rejected.
  * The AR will return an error indicated that the message size is above the limit.
  * If the message size is above 5 MB the AR will not return any error. In order to send messages with sizes above threshold, these messages must be split into chunks with the above limit.
  */
