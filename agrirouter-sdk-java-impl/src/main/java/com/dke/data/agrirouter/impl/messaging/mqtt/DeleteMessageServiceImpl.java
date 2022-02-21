e8ffdf59ea0046197b6fad42642768ff682d3805
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2022-02-21 22:55:33 +0100

    Add functions to delete all messages from the feed. (#177)
    * Add functions to delete all messages from the feed.
    Format.
    
    * Add interface default method to avoid DRY.
    
    * Fix typos.

--- agrirouter-sdk-java-impl/src/main/java/com/dke/data/agrirouter/impl/messaging/mqtt/DeleteMessageServiceImpl.java
+++ agrirouter-sdk-java-impl/src/main/java/com/dke/data/agrirouter/impl/messaging/mqtt/DeleteMessageServiceImpl.java
@@ -1,12 +1,14 @@
 package com.dke.data.agrirouter.impl.messaging.mqtt;
 
 import com.dke.data.agrirouter.api.dto.encoding.EncodedMessage;
+import com.dke.data.agrirouter.api.dto.onboard.OnboardingResponse;
 import com.dke.data.agrirouter.api.exception.CouldNotSendMqttMessageException;
 import com.dke.data.agrirouter.api.messaging.MqttAsyncMessageSendingResult;
 import com.dke.data.agrirouter.api.service.messaging.encoding.EncodeMessageService;
 import com.dke.data.agrirouter.api.service.messaging.mqtt.DeleteMessageService;
 import com.dke.data.agrirouter.api.service.parameters.DeleteMessageParameters;
 import com.dke.data.agrirouter.api.service.parameters.SendMessageParameters;
+import com.dke.data.agrirouter.impl.common.UtcTimeService;
 import com.dke.data.agrirouter.impl.messaging.MessageBodyCreator;
 import com.dke.data.agrirouter.impl.messaging.MessageEncoder;
 import com.dke.data.agrirouter.impl.messaging.MqttService;
@@ -14,12 +16,15 @@ import com.dke.data.agrirouter.impl.messaging.encoding.EncodeMessageServiceImpl;
 import java.util.Collections;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+
+import com.dke.data.agrirouter.impl.messaging.helper.DeleteAllMessagesParameterCreator;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
 import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.jetbrains.annotations.NotNull;
 
 public class DeleteMessageServiceImpl extends MqttService
-    implements DeleteMessageService, MessageBodyCreator, MessageEncoder {
+    implements DeleteMessageService, MessageBodyCreator, MessageEncoder, DeleteAllMessagesParameterCreator {
 
   private final EncodeMessageService encodeMessageService = new EncodeMessageServiceImpl();
 
@@ -59,4 +64,19 @@ public class DeleteMessageServiceImpl extends MqttService
   public EncodeMessageService getEncodeMessageService() {
     return encodeMessageService;
   }
+
+  @Override
+  public String deleteAll(OnboardingResponse onboardingResponse) {
+    final DeleteMessageParameters deleteMessageParameters =
+        createMessageParametersToDeleteAllMessages(onboardingResponse);
+    return send(deleteMessageParameters);
+  }
+
+  @Override
+  public MqttAsyncMessageSendingResult deleteAllAsync(OnboardingResponse onboardingResponse) {
+    final DeleteMessageParameters deleteMessageParameters =
+        createMessageParametersToDeleteAllMessages(onboardingResponse);
+    return sendAsync(deleteMessageParameters);
+  }
+
 }
--- agrirouter-sdk-java-impl/src/main/java/com/dke/data/agrirouter/impl/messaging/rest/DeleteMessageServiceImpl.java
+++ agrirouter-sdk-java-impl/src/main/java/com/dke/data/agrirouter/impl/messaging/rest/DeleteMessageServiceImpl.java
@@ -1,20 +1,24 @@
 package com.dke.data.agrirouter.impl.messaging.rest;
 
 import com.dke.data.agrirouter.api.dto.encoding.EncodedMessage;
+import com.dke.data.agrirouter.api.dto.onboard.OnboardingResponse;
 import com.dke.data.agrirouter.api.messaging.HttpAsyncMessageSendingResult;
 import com.dke.data.agrirouter.api.messaging.MessageSendingResponse;
 import com.dke.data.agrirouter.api.service.messaging.encoding.EncodeMessageService;
 import com.dke.data.agrirouter.api.service.messaging.http.DeleteMessageService;
 import com.dke.data.agrirouter.api.service.parameters.DeleteMessageParameters;
 import com.dke.data.agrirouter.api.service.parameters.SendMessageParameters;
+import com.dke.data.agrirouter.impl.common.UtcTimeService;
 import com.dke.data.agrirouter.impl.messaging.MessageEncoder;
 import com.dke.data.agrirouter.impl.messaging.encoding.EncodeMessageServiceImpl;
+import com.dke.data.agrirouter.impl.messaging.helper.DeleteAllMessagesParameterCreator;
 import com.dke.data.agrirouter.impl.validation.ResponseValidator;
 import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
+import org.jetbrains.annotations.NotNull;
 
 public class DeleteMessageServiceImpl
-    implements DeleteMessageService, MessageSender, ResponseValidator, MessageEncoder {
+    implements DeleteMessageService, MessageSender, ResponseValidator, MessageEncoder, DeleteAllMessagesParameterCreator {
 
   private final EncodeMessageService encodeMessageService;
 
@@ -52,4 +56,19 @@ public class DeleteMessageServiceImpl
   public EncodeMessageService getEncodeMessageService() {
     return this.encodeMessageService;
   }
+
+  @Override
+  public String deleteAll(OnboardingResponse onboardingResponse) {
+    final DeleteMessageParameters deleteMessageParameters =
+        createMessageParametersToDeleteAllMessages(onboardingResponse);
+    return send(deleteMessageParameters);
+  }
+
+  @Override
+  public HttpAsyncMessageSendingResult deleteAllAsync(OnboardingResponse onboardingResponse) {
+    final DeleteMessageParameters deleteMessageParameters =
+        createMessageParametersToDeleteAllMessages(onboardingResponse);
+    return sendAsync(deleteMessageParameters);
+  }
+
 }
