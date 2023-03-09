3cb6de1f218bf5c3892052eb0675f0725caed4dd
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-03-09 18:26:31 +0100

    Extract constants. Optimize OutputStream creation.

---
+++ agrirouter-sdk-java-api/src/main/java/com/dke/data/agrirouter/api/env/Constants.java
@@ -0,0 +1,11 @@
+package com.dke.data.agrirouter.api.env;
+
+/**
+ * Constants for the SDK.
+ */
+public interface Constants {
+    String DEFAULT_PASSWORD = "changeit";
+    String PROTOCOL = "TLSv1.2";
+    String CERT_ALIAS = "cert-alias";
+    String KEY_ALIAS = "key-alias";
+}
--- agrirouter-sdk-java-convenience/src/main/java/com/dke/data/agrirouter/convenience/mqtt/client/MqttOptionService.java
+++ agrirouter-sdk-java-convenience/src/main/java/com/dke/data/agrirouter/convenience/mqtt/client/MqttOptionService.java
@@ -3,18 +3,20 @@ package com.dke.data.agrirouter.convenience.mqtt.client;
 import com.dke.data.agrirouter.api.dto.onboard.OnboardingResponse;
 import com.dke.data.agrirouter.api.dto.onboard.RouterDevice;
 import com.dke.data.agrirouter.api.enums.CertificationType;
+import com.dke.data.agrirouter.api.env.Constants;
 import com.dke.data.agrirouter.api.env.Environment;
 import com.dke.data.agrirouter.api.exception.CouldNotCreateMqttOptionException;
 import com.dke.data.agrirouter.impl.EnvironmentalService;
 import com.dke.data.agrirouter.impl.common.ssl.KeyStoreCreationService;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
-import javax.net.SocketFactory;
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 
 /** Creating MQTT options for the MQTT client. */
 public class MqttOptionService extends EnvironmentalService {
@@ -90,7 +92,7 @@ public class MqttOptionService extends EnvironmentalService {
       if (certificationType == CertificationType.PEM) {
         kmf.init(
             this.keyStoreCreationService.createAndReturnKeystoreFromPEM(certificate, password),
-            "changeit".toCharArray());
+                Constants.DEFAULT_PASSWORD.toCharArray());
       } else {
         if (certificationType == CertificationType.P12) {
           kmf.init(
@@ -98,7 +100,7 @@ public class MqttOptionService extends EnvironmentalService {
               password.toCharArray());
         }
       }
-      SSLContext context = SSLContext.getInstance("TLSv1.2");
+      SSLContext context = SSLContext.getInstance(Constants.PROTOCOL);
       context.init(kmf.getKeyManagers(), null, null);
       return context.getSocketFactory();
     } catch (NoSuchAlgorithmException e) {
