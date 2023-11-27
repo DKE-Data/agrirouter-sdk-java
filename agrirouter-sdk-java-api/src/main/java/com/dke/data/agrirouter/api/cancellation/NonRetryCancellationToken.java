955192885a81a925493aabc0d4920e7fa9c2584f
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-11-27 10:59:47 +0100

    Update build scripts and dependencies in pom.xml
    This commit removes outdated or redundant properties in pom.xml, updates the Java version to 17 and hard-codes versions for each dependency in pom.xml directly, rather than referencing them as properties. This was done to improve readability and maintainability of the file.
    
    Additionally, the Java version on the GitHub workflows has been updated to align with the version in pom.xml, and unnecessary workflow steps have been removed. The commit also introduces a NonRetryCancellationToken class for non-cancellable tasks.

---
+++ agrirouter-sdk-java-api/src/main/java/com/dke/data/agrirouter/api/cancellation/NonRetryCancellationToken.java
@@ -0,0 +1,25 @@
+package com.dke.data.agrirouter.api.cancellation;
+
+/**
+ * Non-retry implementation of the cancellation token.
+ */
+public class NonRetryCancellationToken implements CancellationToken {
+
+    public NonRetryCancellationToken() {
+    }
+
+    @Override
+    public boolean isNotCancelled() {
+        return true;
+    }
+
+    @Override
+    public void nextStep() {
+        // Do nothing
+    }
+
+    @Override
+    public void waitIfNotCancelled() {
+        // Do nothing
+    }
+}
