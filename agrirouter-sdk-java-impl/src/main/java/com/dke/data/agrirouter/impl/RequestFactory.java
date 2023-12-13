fb319b3e4edc4371dc619f5291c7a4bfb8fd2406
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-12-13 10:01:37 +0100

    Update request setup and keystore creation methods
    Refined the request factory to improve the keystore creation by considering the certificate type. Also, adjusted onboarding parameters and responses based on new device and sensor alternate IDs and amended registration code. This results in more straightforward implementation of SSL/TLS client configuration.

--- agrirouter-sdk-java-impl/src/main/java/com/dke/data/agrirouter/impl/RequestFactory.java
+++ agrirouter-sdk-java-impl/src/main/java/com/dke/data/agrirouter/impl/RequestFactory.java
@@ -9,156 +9,159 @@ import com.dke.data.agrirouter.impl.common.ssl.KeyStoreCreationService;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.Invocation;
-import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.HttpHeaders;
 import jakarta.ws.rs.core.MediaType;
-import java.security.KeyStore;
 import org.glassfish.jersey.client.ClientConfig;
 import org.glassfish.jersey.client.ClientProperties;
 import org.glassfish.jersey.logging.LoggingFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/** Factory to encapsulate the requests against the agrirouter */
+import java.security.KeyStore;
+
+/**
+ * Factory to encapsulate the requests against the agrirouter
+ */
 public final class RequestFactory {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(RequestFactory.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(RequestFactory.class);
 
-  /** Hidden constructor. */
-  private RequestFactory() {
-    // NOP
-  }
-
-  /**
-   * Creating a request with SSL configuration using the PEM and KEY files from the agrirouter.
-   *
-   * @param url -
-   * @param certificate -
-   * @param password -
-   * @return Builder -
-   */
-  public static Invocation.Builder securedRequest(
-      String url, String certificate, String password, CertificationType certificationType) {
-    ClientConfig clientConfig = new ClientConfig();
-    KeyStore keyStore = createKeyStore(certificate, password, certificationType);
-    Client client = createClient(clientConfig, keyStore, password, certificationType);
-    if (Environment.httpRequestLoggingEnabled()) {
-      client.property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_CLIENT, "INFO");
-    } else {
-      LOGGER.debug(
-          "Request logging is currently disabled. If you want to enable it, please set '{}'.",
-          Environment.ENABLE_HTTP_REQUEST_LOGGING);
+    /**
+     * Hidden constructor.
+     */
+    private RequestFactory() {
+        // NOP
     }
-    WebTarget target = client.target(url);
-    Invocation.Builder request = target.request(MediaType.APPLICATION_JSON_TYPE);
-    request.accept(MediaType.APPLICATION_JSON_TYPE);
-    return request;
-  }
 
-  private static Client createClient(
-      ClientConfig clientConfig,
-      KeyStore keyStore,
-      String password,
-      CertificationType certificationType) {
-    try {
-      switch (certificationType) {
-        case PEM:
-          return ClientBuilder.newBuilder()
-              .withConfig(clientConfig)
-              .keyStore(keyStore, Constants.DEFAULT_PASSWORD)
-              .build();
-        case P12:
-          return ClientBuilder.newBuilder()
-              .withConfig(clientConfig)
-              .keyStore(keyStore, password)
-              .build();
-        default:
-          throw new CertificationTypeNotSupportedException(certificationType);
-      }
-    } catch (Exception e) {
-      throw new CouldNotCreateDynamicKeyStoreException(e);
+    /**
+     * Creating a request with SSL configuration using the PEM and KEY files from the agrirouter.
+     *
+     * @param url         -
+     * @param certificate -
+     * @param password    -
+     * @return Builder -
+     */
+    public static Invocation.Builder securedRequest(
+            String url, String certificate, String password, CertificationType certificationType) {
+        var clientConfig = new ClientConfig();
+        var keyStore = createKeyStore(certificate, password, certificationType);
+        try (var client = createClient(clientConfig, keyStore, password, certificationType)) {
+            if (Environment.httpRequestLoggingEnabled()) {
+                client.property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_CLIENT, "INFO");
+            } else {
+                LOGGER.debug(
+                        "Request logging is currently disabled. If you want to enable it, please set '{}'.",
+                        Environment.ENABLE_HTTP_REQUEST_LOGGING);
+            }
+            var target = client.target(url);
+            var request = target.request(MediaType.APPLICATION_JSON_TYPE);
+            request.accept(MediaType.APPLICATION_JSON_TYPE);
+            return request;
+        }
     }
-  }
 
-  private static KeyStore createKeyStore(
-      String x509Certificate, String password, CertificationType certificationType) {
-    KeyStoreCreationService keyStoreCreationService = new KeyStoreCreationService();
-    try {
-      switch (certificationType) {
-        case PEM:
-          return keyStoreCreationService.createAndReturnKeystoreFromPEM(x509Certificate, password);
-        case P12:
-          return keyStoreCreationService.createAndReturnKeystoreFromP12(x509Certificate, password);
-        default:
-          throw new CertificationTypeNotSupportedException(certificationType);
-      }
-    } catch (Exception e) {
-      throw new CouldNotCreateDynamicKeyStoreException(e);
+    private static Client createClient(
+            ClientConfig clientConfig,
+            KeyStore keyStore,
+            String password,
+            CertificationType certificationType) {
+        try {
+            return switch (certificationType) {
+                case PEM -> ClientBuilder.newBuilder()
+                        .withConfig(clientConfig)
+                        .keyStore(keyStore, Constants.DEFAULT_PASSWORD)
+                        .build();
+                case P12 -> ClientBuilder.newBuilder()
+                        .withConfig(clientConfig)
+                        .keyStore(keyStore, password)
+                        .build();
+            };
+        } catch (Exception e) {
+            throw new CouldNotCreateDynamicKeyStoreException(e);
+        }
     }
-  }
 
-  /**
-   * Setting the 'reg_access_token' within the header.
-   *
-   * @param url -
-   * @param accessToken -
-   * @return Builder -
-   */
-  public static Invocation.Builder bearerTokenRequest(String url, String accessToken) {
-    Client client = ClientBuilder.newClient();
-    client.property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_CLIENT, "INFO");
-    WebTarget target = client.target(url);
-    Invocation.Builder request = target.request(MediaType.APPLICATION_JSON_TYPE);
-    request.accept(MediaType.APPLICATION_JSON_TYPE);
-    request.header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+    private static KeyStore createKeyStore(
+            String x509Certificate, String password, CertificationType certificationType) {
+        var keyStoreCreationService = new KeyStoreCreationService();
+        try {
+            switch (certificationType) {
+                case PEM:
+                    return keyStoreCreationService.createAndReturnKeystoreFromPEM(x509Certificate, password);
+                case P12:
+                    return keyStoreCreationService.createAndReturnKeystoreFromP12(x509Certificate, password);
+                default:
+                    throw new CertificationTypeNotSupportedException(certificationType);
+            }
+        } catch (Exception e) {
+            throw new CouldNotCreateDynamicKeyStoreException(e);
+        }
+    }
 
-    return request;
-  }
+    /**
+     * Setting the 'reg_access_token' within the header.
+     *
+     * @param url         -
+     * @param accessToken -
+     * @return Builder -
+     */
+    public static Invocation.Builder bearerTokenRequest(String url, String accessToken) {
+        try (var client = ClientBuilder.newClient()) {
+            client.property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_CLIENT, "INFO");
+            var target = client.target(url);
+            var request = target.request(MediaType.APPLICATION_JSON_TYPE);
+            request.accept(MediaType.APPLICATION_JSON_TYPE);
+            request.header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+            return request;
+        }
+    }
 
-  /**
-   * Setting applicationID and Signature within the header.
-   *
-   * @param url -
-   * @param applicationId -
-   * @param signature -
-   * @return Builder -
-   */
-  public static Invocation.Builder signedDeleteRequest(
-      String url, String applicationId, String signature) {
-    Client client = ClientBuilder.newClient();
-    client.property(ClientProperties.SUPPRESS_HTTP_COMPLIANCE_VALIDATION, true);
-    client.property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_CLIENT, "INFO");
-    WebTarget target = client.target(url);
-    Invocation.Builder request = target.request(MediaType.APPLICATION_JSON_TYPE);
-    request.accept(MediaType.APPLICATION_JSON_TYPE);
-    request.header(AgrirouterHttpHeader.APPLICATION_ID, applicationId);
-    request.header(AgrirouterHttpHeader.SIGNATURE, signature);
+    /**
+     * Setting applicationID and Signature within the header.
+     *
+     * @param url           -
+     * @param applicationId -
+     * @param signature     -
+     * @return Builder -
+     */
+    public static Invocation.Builder signedDeleteRequest(
+            String url, String applicationId, String signature) {
+        try (var client = ClientBuilder.newClient()) {
+            client.property(ClientProperties.SUPPRESS_HTTP_COMPLIANCE_VALIDATION, true);
+            client.property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_CLIENT, "INFO");
+            var target = client.target(url);
+            var request = target.request(MediaType.APPLICATION_JSON_TYPE);
+            request.accept(MediaType.APPLICATION_JSON_TYPE);
+            request.header(AgrirouterHttpHeader.APPLICATION_ID, applicationId);
+            request.header(AgrirouterHttpHeader.SIGNATURE, signature);
 
-    return request;
-  }
+            return request;
+        }
+    }
 
-  /**
-   * Setting the 'reg_access_token' within the header.
-   *
-   * @param url -
-   * @param accessToken -
-   * @return Builder -
-   */
-  public static Invocation.Builder bearerTokenRequest(
-      String url, String accessToken, String applicationId, String signature) {
-    Client client = ClientBuilder.newClient();
-    client.property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_CLIENT, "INFO");
-    WebTarget target = client.target(url);
-    Invocation.Builder request = target.request(MediaType.APPLICATION_JSON_TYPE);
-    request.accept(MediaType.APPLICATION_JSON_TYPE);
-    request.header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
-    request.header(AgrirouterHttpHeader.APPLICATION_ID, applicationId);
-    request.header(AgrirouterHttpHeader.SIGNATURE, signature);
-    return request;
-  }
+    /**
+     * Setting the 'reg_access_token' within the header.
+     *
+     * @param url         -
+     * @param accessToken -
+     * @return Builder -
+     */
+    public static Invocation.Builder bearerTokenRequest(
+            String url, String accessToken, String applicationId, String signature) {
+        try (var client = ClientBuilder.newClient()) {
+            client.property(LoggingFeature.LOGGING_FEATURE_LOGGER_LEVEL_CLIENT, "INFO");
+            var target = client.target(url);
+            var request = target.request(MediaType.APPLICATION_JSON_TYPE);
+            request.accept(MediaType.APPLICATION_JSON_TYPE);
+            request.header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
+            request.header(AgrirouterHttpHeader.APPLICATION_ID, applicationId);
+            request.header(AgrirouterHttpHeader.SIGNATURE, signature);
+            return request;
+        }
+    }
 
-  public static class AgrirouterHttpHeader {
-    public static final String APPLICATION_ID = "X-Agrirouter-ApplicationId";
-    public static final String SIGNATURE = "X-Agrirouter-Signature";
-  }
+    public static class AgrirouterHttpHeader {
+        public static final String APPLICATION_ID = "X-Agrirouter-ApplicationId";
+        public static final String SIGNATURE = "X-Agrirouter-Signature";
+    }
 }
