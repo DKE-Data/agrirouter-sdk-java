745185f1898a39d7a705fa1b64b95091057d784f
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-12-13 11:27:29 +0100

    Remove specific exception classes
    The CouldNotFetchCookiesException, CouldNotGetRegistrationCodeException, and CouldNotOffboardVirtualCommunicationUnitException classes have been removed. These specific exception handling classes were no longer needed or used in the codebase.

--- agrirouter-sdk-java-api/src/main/java/com/dke/data/agrirouter/api/exception/CouldNotFetchCookiesException.java
+++
@@ -1,9 +0,0 @@
-package com.dke.data.agrirouter.api.exception;
-
-public class CouldNotFetchCookiesException extends RuntimeException {
-
-    public CouldNotFetchCookiesException(Throwable cause) {
-
-        super(cause);
-    }
-}
