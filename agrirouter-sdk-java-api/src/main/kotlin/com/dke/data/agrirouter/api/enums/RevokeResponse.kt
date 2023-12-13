0b55b5445e9abc7fb570cc047cacc748731cbec3
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-12-13 11:29:20 +0100

    improves code readability and ensures the correct use of the class, by using a more appropriate function (`entries.find` instead of `RevokeResponse.values().find`) to find values within the RevokeResponse enumeration.

--- agrirouter-sdk-java-api/src/main/kotlin/com/dke/data/agrirouter/api/enums/RevokeResponse.kt
+++ agrirouter-sdk-java-api/src/main/kotlin/com/dke/data/agrirouter/api/enums/RevokeResponse.kt
@@ -6,6 +6,6 @@ enum class RevokeResponse(val key: Int) {
     UNAUTHORIZED(401);
 
     companion object Filter {
-        fun valueOf(value: Int): RevokeResponse? = RevokeResponse.values().find { it.key == value }
+        fun valueOf(value: Int): RevokeResponse? = entries.find { it.key == value }
     }
 }
