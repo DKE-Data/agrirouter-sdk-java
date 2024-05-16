6c2dbdb6f412f0e3a76d65582aaebc0f484eb715
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-05-16 10:57:54 +0200

    Update onboarding parameters and response for farming software tests
    The onboarding parameters and response for the farming software tests have been updated. This includes changes to the registration code and a new response in the farming-software.json file which includes new id values and URLs.

--- agrirouter-sdk-java-tests/src/test/java/com/dke/data/agrirouter/test/fixture/FarmingSoftwareFixture.java
+++ agrirouter-sdk-java-tests/src/test/java/com/dke/data/agrirouter/test/fixture/FarmingSoftwareFixture.java
@@ -38,7 +38,7 @@ class FarmingSoftwareFixture extends AbstractIntegrationTest {
         OnboardingService onboardingService = new OnboardingServiceImpl(new QA() {
         });
         SecuredOnboardingParameters onboardingParameters = new SecuredOnboardingParameters();
-        onboardingParameters.setRegistrationCode("cf3a08a02f");
+        onboardingParameters.setRegistrationCode("6a049384e3");
         onboardingParameters.setApplicationId(farmingSoftware.getApplicationId());
         onboardingParameters.setCertificationVersionId(farmingSoftware.getCertificationVersionId());
         onboardingParameters.setCertificationType(CertificationType.P12);
