75ecdd833cbff5ee103834cd07163c071f85e956
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-05-16 13:32:08 +0200

    Deprecate SetCapabilityServiceImpl constructor in SDK
    The SetCapabilityServiceImpl constructor that takes an Environment parameter has been deprecated. A warning message has been added to indicate that this constructor is deprecated, and it should not be used moving forward as it will be removed in a future release. This change aims to streamline the use of the SetCapabilityService requiring no environment parameters.

--- agrirouter-sdk-java-impl/src/main/java/com/dke/data/agrirouter/impl/messaging/rest/SetCapabilityServiceImpl.java
+++ agrirouter-sdk-java-impl/src/main/java/com/dke/data/agrirouter/impl/messaging/rest/SetCapabilityServiceImpl.java
@@ -1,5 +1,6 @@
 package com.dke.data.agrirouter.impl.messaging.rest;
 
+import com.dke.data.agrirouter.api.env.Environment;
 import com.dke.data.agrirouter.api.messaging.HttpAsyncMessageSendingResult;
 import com.dke.data.agrirouter.api.service.messaging.encoding.EncodeMessageService;
 import com.dke.data.agrirouter.api.service.messaging.http.SetCapabilityService;
@@ -8,13 +9,31 @@ import com.dke.data.agrirouter.api.service.parameters.SetCapabilitiesParameters;
 import com.dke.data.agrirouter.impl.messaging.MessageEncoder;
 import com.dke.data.agrirouter.impl.messaging.encoding.EncodeMessageServiceImpl;
 import com.dke.data.agrirouter.impl.validation.ResponseValidator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 
 public class SetCapabilityServiceImpl
         implements SetCapabilityService, MessageSender, MessageEncoder, ResponseValidator {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SetCapabilityServiceImpl.class);
+
     private final EncodeMessageService encodeMessageService;
+
+    /**
+     * Implements the SetCapabilityService interface and provides methods for sending
+     * capability data to the agrirouter.
+     *
+     * @deprecated This class is deprecated since version 3.1.0 and will be removed in a future release.
+     * The environment parameter is no longer necessary to use the service.
+     * Please use the default constructor instead.
+     */
+    @Deprecated(since = "3.1.0", forRemoval = true)
+    public SetCapabilityServiceImpl(Environment ignoredEnvironment) {
+        LOGGER.warn("This constructor is deprecated. The environment is not necessary to use the service. Within the next major release this constructor will be removed.");
+        this.encodeMessageService = new EncodeMessageServiceImpl();
+    }
 
     public SetCapabilityServiceImpl() {
         this.encodeMessageService = new EncodeMessageServiceImpl();
