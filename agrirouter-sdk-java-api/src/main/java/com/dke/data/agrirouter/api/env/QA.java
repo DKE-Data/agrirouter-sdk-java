8f51d4da6076e83f110447df69604b7736db56e3
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-07-21 20:49:16 +0200

    Deprecate QA environment class in agrirouter SDK
    The QA class in the agrirouter SDK has been marked as deprecated indicating its imminent removal in future versions. Moving forward, the default agrirouter environment should be used.

--- agrirouter-sdk-java-api/src/main/java/com/dke/data/agrirouter/api/env/QA.java
+++ agrirouter-sdk-java-api/src/main/java/com/dke/data/agrirouter/api/env/QA.java
@@ -2,7 +2,11 @@ package com.dke.data.agrirouter.api.env;
 
 /**
  * Abstraction of the QA environment, currently no overrides because the default is QA already.
+ *
+ * @deprecated This class is deprecated and will be removed in future versions.
+ * The new agrirouter environment will be the default to use
  */
+@Deprecated(since = "3.2.0", forRemoval = true)
 public abstract class QA implements Environment {
 
     private static final String ENV_BASE_URL = "https://agrirouter-qa.cfapps.eu10.hana.ondemand.com";
