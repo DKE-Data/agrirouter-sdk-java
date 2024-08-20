c636aedea94c21a270e0c3119eed2df38cb6b46c
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-08-20 10:26:02 +0200

    Refactor MQTT test cases for better integration
    Updated `PingServiceTest` to extend `AbstractIntegrationTest` and added necessary imports and services for MQTT testing. Modified `MqttCommunicationUnitFixture` to use MQTT instead of REST and added a logger for message delivery confirmation. Adjusted onboarding response JSON to match the new configuration.

--- agrirouter-sdk-java-tests/src/test/java/com/dke/data/agrirouter/test/messaging/mqtt/PingServiceTest.java
+++ agrirouter-sdk-java-tests/src/test/java/com/dke/data/agrirouter/test/messaging/mqtt/PingServiceTest.java
@@ -1,11 +1,24 @@
 package com.dke.data.agrirouter.test.messaging.mqtt;
 
+import com.dke.data.agrirouter.api.dto.onboard.OnboardingResponse;
+import com.dke.data.agrirouter.api.env.QA;
+import com.dke.data.agrirouter.convenience.mqtt.client.MqttClientService;
+import com.dke.data.agrirouter.convenience.mqtt.client.MqttOptionService;
+import com.dke.data.agrirouter.test.AbstractIntegrationTest;
+import com.dke.data.agrirouter.test.OnboardingResponseRepository;
 import org.junit.jupiter.api.Test;
 
-class PingServiceTest {
+class PingServiceTest extends AbstractIntegrationTest {
 
     @Test
-    void sendHealthMessageToPingEndpoint() {
+    void sendHealthMessageToPingEndpoint() throws Throwable {
+        OnboardingResponse onboardingResponse = OnboardingResponseRepository.read(OnboardingResponseRepository.Identifier.MQTT_COMMUNICATION_UNIT);
+
+        MqttOptionService mqttOptionService = new MqttOptionService(new QA() {
+        });
+        MqttClientService mqttClientService = new MqttClientService(new QA() {
+        });
     }
+
 
 }
