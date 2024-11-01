ac46f1959b32f99b929899a8062af307e7ca4e4e
Sascha Dömer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2024-11-01 14:06:34 +0100

    Refactor MQTT client handling
    Introduced MqttClientWrapper and PahoMqttClientWrapper to encapsulate MQTT client operations, improving abstraction and sanitarizing exception handling. This also simplifies the CloudOnboardingServiceImpl class and removes direct dependencies on the Paho MQTT library.

--- agrirouter-sdk-java-impl/src/main/java/com/dke/data/agrirouter/impl/messaging/mqtt/CloudOnboardingServiceImpl.java
+++ agrirouter-sdk-java-impl/src/main/java/com/dke/data/agrirouter/impl/messaging/mqtt/CloudOnboardingServiceImpl.java
@@ -1,7 +1,7 @@
 package com.dke.data.agrirouter.impl.messaging.mqtt;
 
-import com.dke.data.agrirouter.api.exception.CouldNotSendMqttMessageException;
 import com.dke.data.agrirouter.api.messaging.MqttAsyncMessageSendingResult;
+import com.dke.data.agrirouter.api.mqtt.PahoMqttClientWrapper;
 import com.dke.data.agrirouter.api.service.messaging.encoding.EncodeMessageService;
 import com.dke.data.agrirouter.api.service.messaging.mqtt.CloudOnboardingService;
 import com.dke.data.agrirouter.api.service.parameters.CloudOnboardingParameters;
@@ -11,8 +11,6 @@ import com.dke.data.agrirouter.impl.messaging.MessageEncoder;
 import com.dke.data.agrirouter.impl.messaging.MqttService;
 import com.dke.data.agrirouter.impl.messaging.encoding.EncodeMessageServiceImpl;
 import org.eclipse.paho.client.mqttv3.IMqttClient;
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 import java.util.Collections;
 import java.util.Objects;
@@ -25,7 +23,7 @@ public class CloudOnboardingServiceImpl extends MqttService
     private final EncodeMessageService encodeMessageService = new EncodeMessageServiceImpl();
 
     public CloudOnboardingServiceImpl(IMqttClient mqttClient) {
-        super(mqttClient);
+        super(new PahoMqttClientWrapper(mqttClient));
     }
 
     /**
@@ -37,24 +35,20 @@ public class CloudOnboardingServiceImpl extends MqttService
     @Override
     public String send(CloudOnboardingParameters parameters) {
         parameters.validate();
-        try {
-            var encodedMessage = this.encode(parameters);
-            var sendMessageParameters = new SendMessageParameters();
-            sendMessageParameters.setOnboardingResponse(parameters.getOnboardingResponse());
-            sendMessageParameters.setEncodedMessages(
-                    Collections.singletonList(encodedMessage.getEncodedMessage()));
-            var messageAsJson = this.createMessageBody(sendMessageParameters);
-            var payload = messageAsJson.getBytes();
-            this.getMqttClient()
-                    .publish(
-                            Objects.requireNonNull(parameters.getOnboardingResponse())
-                                    .getConnectionCriteria()
-                                    .getMeasures(),
-                            new MqttMessage(payload));
-            return encodedMessage.getApplicationMessageID();
-        } catch (MqttException e) {
-            throw new CouldNotSendMqttMessageException(e);
-        }
+        var encodedMessage = this.encode(parameters);
+        var sendMessageParameters = new SendMessageParameters();
+        sendMessageParameters.setOnboardingResponse(parameters.getOnboardingResponse());
+        sendMessageParameters.setEncodedMessages(
+                Collections.singletonList(encodedMessage.getEncodedMessage()));
+        var messageAsJson = this.createMessageBody(sendMessageParameters);
+        var payload = messageAsJson.getBytes();
+        this.getMqttClient()
+                .publish(
+                        Objects.requireNonNull(parameters.getOnboardingResponse())
+                                .getConnectionCriteria()
+                                .getMeasures(),
+                        payload);
+        return encodedMessage.getApplicationMessageID();
     }
 
     @Override
